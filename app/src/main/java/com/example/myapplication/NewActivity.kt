279aa68ddf6a0efa64924e5d1621a4ad1a537231
B-Yahia <60832087+B-Yahia@users.noreply.github.com>
2020-04-07 08:10:27 -0700

    Update NewActivity.kt

--- app/src/main/java/com/example/myapplication/NewActivity.kt
+++ app/src/main/java/com/example/myapplication/NewActivity.kt
@@ -1,12 +1,40 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_new.*
 
 class NewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
+
+        Cal.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        var dis = findViewById<EditText>(R.id.dist)
+        var disKM = findViewById<Button>(R.id.km)
+        var disML = findViewById<Button>(R.id.miles)
+        var rlt = findViewById<TextView>(R.id.result)
+        val coe1 = 1.60934
+        val coe2 = 0.621371
+        disKM.setOnClickListener( View.OnClickListener{
+            rlt.text= ((dis.text.toString().toInt()*coe1).toString())
+            Toast.makeText( this,rlt.text,Toast.LENGTH_LONG).show()
+        })
+        disML.setOnClickListener( View.OnClickListener{
+            rlt.text= ((dis.text.toString().toInt()*coe2).toString())
+            Toast.makeText( this,rlt.text,Toast.LENGTH_LONG).show()
+        })
+
+
     }
 }
